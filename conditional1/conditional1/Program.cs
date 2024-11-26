d46ad99d93ff4c0b10bc4eea746f0c0b0a52819c
sayaliparbhane <sauparbhane25@gmail.com>
2024-11-26 19:55:44 +0530

    Add files via upload

---
+++ conditional1/conditional1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditional1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter thr no \n");
+            int no=Convert.ToInt32(Console.ReadLine());
+           
+            if(no %2==0)
+            { 
+              
+                Console.WriteLine("given no is :" + no + "No is even\n");
+            }
+            else
+            {
+                Console.WriteLine("given no :"+no +" No is odd \n");
+            }
+                Console.ReadLine();
+
+                    
+        }
+
+    }
+}
